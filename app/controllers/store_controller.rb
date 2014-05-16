8c7a07f0692febde61d7b6062cc5f97df6c90a8e
Jonathan Frankel <frankel.jb@gmail.com>
2014-05-16 12:35:10 +0200

    A stupid cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,16 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+  	get_visit_count
   end
+
+  private 
+
+  	def get_visit_count
+  		if session[:counter].nil?
+  			session[:counter] = 1
+  		else
+  			session[:counter] = session[:counter] + 1
+  		end
+		end
 end
